1a01c8a27b9f62eaa5de6e5c9cd678b3e40ec008
KDH <esonagi123@naver.com>
2023-09-13 13:04:10 +0900

    테스트

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -32,7 +32,7 @@
     <!-- Core CSS -->
     <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
     <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="../assets/css/demo.css" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/demo.css') }}" />
 
     <!-- Vendors CSS -->
     <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
