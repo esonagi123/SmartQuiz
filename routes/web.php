cfe0675491fbe9557c9f3d130fd019db7454556c
ptokos <jiseokchoij@gmail.com>
2023-09-20 13:53:43 +0900

    login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('base');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/forgot-pw', function () {
+    return view('forgot-pw');
+});
