5ab81a797a2081829232f22bc2d1a8d0a7b6a59e
KDH <esonagi123@naver.com>
2023-09-27 15:32:49 +0900

    validator 수정

--- app/Http/Controllers/account/Account.php
+++ app/Http/Controllers/account/Account.php
@@ -26,29 +26,19 @@ class Account extends Controller
     
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-			'nickname' => 'required|max:255',
+        $validator = $request->validate([
 			'uid' => 'required|unique:members|max:255',
 			'email' => 'required|unique:members|email|max:255',
 			'password' => 'required|confirmed',
+            'nickname' => 'required|max:255',
 		], [
 			'nickname.required' => '닉네임을 입력하세요.',
 			'nickname.max' => '닉네임은 255자 이내로 입력하세요.',
@@ -86,46 +76,21 @@ class Account extends Controller
 		}
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
