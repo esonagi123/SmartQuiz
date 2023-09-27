a3e4a32d9cd655c33a0bc177d35e63a078b8bd7b
KDH <esonagi123@naver.com>
2023-09-27 15:27:33 +0900

    .

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     use HasFactory;
+	protected $table = 'members';
+	protected $primaryKey = 'id';
+    protected $fillable = [
+        'uid', 'nickname', 'email', 'password'
+    ];
 }
