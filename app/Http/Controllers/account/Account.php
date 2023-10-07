b4ecec8a267b82821fd621474b7130848e93e9a4
KDH <esonagi123@naver.com>
2023-10-07 23:44:50 +0900

    정규식 및 유효성 검사 수정

--- app/Http/Controllers/account/Account.php
+++ app/Http/Controllers/account/Account.php
@@ -35,22 +35,24 @@ class Account extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'uid' => 'required|unique:members|max:255',
+            'uid' => 'required|unique:members|regex:/^[a-z0-9_-]{5,20}$/',
             'email' => 'required|unique:members|email|max:255',
-            'password' => 'required|confirmed',
-            'nickname' => 'required|max:255',
+            'password' => 'required|confirmed|min:8|max:16',
+            'nickname' => 'required|regex:/^[a-zA-Z가-힣]{2,8}$/',
         ], [
             'nickname.required' => '닉네임을 입력하세요.',
-            'nickname.max' => '닉네임은 255자 이내로 입력하세요.',
-            'uid.required' => 'ID를 입력하세요.',
-            'uid.unique' => '이미 사용 중인 ID입니다.',
-            'uid.max' => 'UID는 255자 이내로 입력하세요.',
+            'nickname.regex' => '닉네임은 2~8자 이내의 한글과 영문 대/소문자만 사용 가능합니다.',
+            'uid.required' => '아이디를 입력하세요.',
+            'uid.unique' => '이미 사용 중인 아이디 입니다.',
+            'uid.regex' => '아이디는 영문 소문자, 숫자, 밑줄(_), 하이픈(-)만 사용 가능하며 5~20자 이내로 입력하세요.',
             'email.required' => '이메일을 입력하세요.',
             'email.unique' => '이미 사용 중인 이메일 주소입니다.',
             'email.email' => '올바른 이메일 주소를 입력하세요.',
             'email.max' => '이메일 주소는 255자 이내로 입력하세요.',
             'password.required' => '비밀번호를 입력하세요.',
             'password.confirmed' => '비밀번호 확인이 일치하지 않습니다.',
+            'password.min' => '비밀번호는 8~16자 이내로 입력하세요.',
+            'password.max' => '비밀번호는 8~16자 이내로 입력하세요.',
         ]);
     
         $user = new Member;
