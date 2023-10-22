6499de00ea9fd19c4a0db575b4a4e73453a5a3e7
KDH <esonagi123@naver.com>
2023-10-22 19:37:07 +0900

    405 에러 발생 시 메인으로 리다이렉트

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +39,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return redirect('/quiz'); // 405 오류가 발생하면 /quiz로 리다이렉트
+        }
+    
+        return parent::render($request, $exception);
     }
 }
